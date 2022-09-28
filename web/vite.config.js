c0a4493836e87b4a1d323007f1c7f39b9c62f453
jmnote <opcore@gmail.com>
2022-09-28 07:23:43 +0000

    first commit

---
+++ web/vite.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  clearScreen: false,
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false,
+      protocol: 'ws',
+      host: 'venti.scpmgmt-dev.ske.kr-east.scp-in.com',
+      clientPort: 80,
+    },
+    host: true,
+    proxy: {
+      '/user': {
+        target: 'http://localhost:8080',
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+      },
+    },
+  },
+})
